3a55aca69868709f12c4533a75542e9c06c22e0c
Ishu Bala <cseishugarg@gmail.com>
2024-08-06 19:27:26 -0400

    dictionary in list

--- list.py
+++ list.py
@@ -1,2 +1,14 @@
-import random
-print("Hello World!!")
+eatables=[ {
+            "fruits":['apple','orange','pear','berry'],
+            "vegetables":['pumpkin','onion']
+            },
+           {
+            "fruits":['banana','strawberry','rasberry'],
+            "vegetables":['paneer','peas']
+            }
+        ]
+# for i in range(2):
+#     print(eatables[i]["fruits"][0])
+
+for item in eatables:
+    print(item["vegetables"][0])
